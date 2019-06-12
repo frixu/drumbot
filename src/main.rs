69ded5e2a333a67bac76f732a273aa9a51f14e0e
Frixu <frixu@frixu.me>
2019-06-12 13:46:24 +0200

    Make Rodio play a single sound

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 mod structs;
 
-use std::{collections::HashMap, clone::Clone, io::BufRead};
+use std::{collections::HashMap, clone::Clone, io::{BufRead, BufReader}, fs::File};
+use std::{time::Duration, thread};
 use reqwest::{Client, StatusCode};
+use rodio::{Source, Sink};
 
 const ENDPOINT: &str = "https://api.noopschallenge.com/drumbot/patterns/";
 
 fn main() {
     let mut patterns = HashMap::new();
+    let device = rodio::default_output_device().unwrap();
     let client = Client::new();
     
     let mut list = client.get(ENDPOINT).send().unwrap();
@@ -29,9 +32,15 @@ fn main() {
             let url = format!("{}{}", ENDPOINT, buffer);
             let new_pattern = client.get(&url[..]).send().unwrap().text().unwrap();
             let pattern : structs::Pattern = serde_json::from_str(&new_pattern).unwrap();
-            patterns.insert(buffer, pattern);
+            patterns.insert(buffer.clone(), pattern);
         }
         println!("Here we go!");
+        let pattern = patterns.get(&buffer).unwrap();
+        let first_track = &pattern.tracks[0];
+        let file = File::open(format!("samples/{}.wav", first_track.instrument.clone())).unwrap();
+        let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
+        rodio::play_raw(&device, source.convert_samples());
+        thread::sleep(Duration::from_secs(86400));
     }
     else {
         println!("No such pattern exists!");
